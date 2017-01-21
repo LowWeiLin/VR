38befee5a64d36c643a0132774f3b6d9e3d70ccb
Low WeiLin <lwl1991@gmail.com>
2017-01-21 16:36:07 +0800

    Add a plane, toggle VR mode by pressing 'v'

---
+++ Assets/ToggleVR.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+
+public class ToggleVR : MonoBehaviour
+{
+	public void Start()
+	{
+		VRSettings.enabled = true;
+		Debug.Log("Changed VRSettings.enabled to:"+VRSettings.enabled);
+	}
+
+	//Example of toggling VRSettings
+	private void Update ()
+	{
+		//If V is pressed, toggle VRSettings.enabled
+		if (Input.GetKeyDown(KeyCode.V))
+		{
+			VRSettings.enabled = !VRSettings.enabled;
+			Debug.Log("Changed VRSettings.enabled to:"+VRSettings.enabled);
+		}	
+	}
+}
