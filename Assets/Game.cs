19d015aa0c075a28f05a439acf166794008b2e15
Darius Foo <darius.foo.tw@gmail.com>
2017-01-22 20:12:51 +0800

    Make gaze trigger work
    The problem was that Standalone Input was ordered before Gaze
    Input in the global EventSystem.
    
    Also makes the back button quit.

---
+++ Assets/Game.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	void LateUpdate() {
+		GvrViewer.Instance.UpdateState();
+		if (GvrViewer.Instance.BackButtonPressed) {
+			Application.Quit();
+		}
+	}
+}
