45d4c4e25d3f4aa180b3620424b3ac8e8be3716b
Low WeiLin <lwl1991@gmail.com>
2017-01-22 09:26:11 +0800

    Add some scripts

---
+++ Assets/ColorSetter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorSetter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Renderer> ().material.color = new Color(0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/EnemyTarget.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * When player looks to EnemyTarget, lockOn value increases
+ * When lockOn value == lockOnMax, target is locked
+ * 
+ * If target is not locked and player is not looking, lockOn value drops to 0
+ *
+ * If lockOn value not 0, show 2D UI to show lockOn value
+ *
+ */
+public class EnemyTarget : MonoBehaviour {
+
+	int lockOnValue = 0;
+	int lockOnMax = 100;
+	bool locked = false;
+
+	bool lookedAt = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (lookedAt && lockOnValue <= lockOnMax) {
+			lockOnValue += 1;
+		}
+		if (!lookedAt && !locked) {
+			lockOnValue -= 1;
+		}
+
+		if (lockOnValue >= lockOnMax) {
+			locked = true;
+		}
+
+		lookedAt = false;
+	}
+
+	void AimedAt() {
+		lookedAt = true;
+	}
+
+}
